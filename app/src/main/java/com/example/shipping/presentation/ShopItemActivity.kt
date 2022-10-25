ea1247c7bffe388021eb523c0534f1da7a287dd0
iliassemeonidis <paleologos@mail.ru>
2022-10-25 19:03:47 +0300

    новый экран

---
+++ app/src/main/java/com/example/shipping/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shipping.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shipping.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
